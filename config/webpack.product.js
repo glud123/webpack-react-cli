adf179fa08b620e139a559f435ce121cc703b8b5
glud123 <glud123@163.com>
2018-12-27 10:08:30 +0800

    *update

--- config/webpack.product.js
+++ config/webpack.product.js
@@ -3,7 +3,7 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const cleanPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = (dirname) => {
     console.log("production >>>>>>>>>>>>>>>>>>>>>>>>>>>");
     return merge(common(dirname), {
@@ -32,9 +32,9 @@ module.exports = (dirname) => {
                 filename: "[name].[hash].css",
                 chunkFilename: "[id].[hash].css"
             }),
-            new CopyWebpackPlugin([
-                { from: dirname + "/src/assets", to: "./assets" }
-            ])
+//             new CopyWebpackPlugin([
+//                 { from: dirname + "/src/assets", to: "./assets" }
+//             ])
         ]
     });
 };
